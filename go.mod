aad0de192caabd079d6560e893a7a14211c3dea7
jeruktutut2 <jeruktutut2@gmail.com>
2022-03-14 22:02:33 +0800

    add http server

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jeruktutut2/backend-user
+
+go 1.16
+
+require github.com/julienschmidt/httprouter v1.3.0
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	fmt.Println("tes")
+
+	router := httprouter.New()
+
+	server := &http.Server{
+		Addr:    ":10001",
+		Handler: router,
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	log.Println("Server Started")
+
+	<-done
+	log.Println("Server Stopped")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown Failed: %+v", err)
+	}
+	log.Println("Server Exited Properly")
+}
